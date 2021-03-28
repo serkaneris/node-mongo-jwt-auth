57a6a317fa9c6de5141e7a47430c0587b160e5f2
serkaneris <serkan_eris@hotmail.com>
2021-03-29 01:04:35 +0300

    add register route

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express= require('express')
 const path = require('path')
 
 const app = express()
+app.use(express.json());
 app.use('/',express.static(path.join(__dirname ,'static')))
+
+app.post('/api/register', async (req,res,next)=>{
+    console.log(req.body.username)
+})
 
 app.listen(8080, ()=>{
     console.log('Server Ready! port:8080')
