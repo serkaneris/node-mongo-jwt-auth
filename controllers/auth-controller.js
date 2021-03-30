63694a3dd849729b196b840ee6fdc873c80d8276
serkaneris <serkan_eris@hotmail.com>
2021-03-30 19:02:07 +0300

    add login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 exports.register = async(req,res,next)=>{
     try{
@@ -14,6 +15,42 @@ exports.register = async(req,res,next)=>{
             result:result
         })
     }catch (err){
+        next(err)
+    }
+}
+
+exports.login = async(req,res,next) => {
+    try{
+        //1. Find User
+        const user = await User.findOne({username:req.body.username})
+        if(!user){
+            const error = new Error('User not found.')
+            error.statusCode = 401
+            throw error
+        }
+
+        //2. Compare password
+        const isPassOK = await bcrypt.compare(req.body.password,user.password)
+        if(!isPassOK){
+            const error = new Error('Wrong password.')
+            error.statusCode = 401
+            throw error
+        }
+
+        //3.Create token
+        const token = jwt.sign({
+            username:user.username,
+            userId:user._id.toString()
+        },
+        'writeyoursupersecretkey',
+        {expiresIn:'1h'})
+
+        res.status(200).json({
+            message:'Login success.',
+            userId:user._id.toString(),
+            token:token
+        })
+    }catch(err){
         next(err)
     }
 }
